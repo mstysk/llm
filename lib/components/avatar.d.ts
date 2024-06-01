ff483af1c789f646eb83cdae63a401ca1ae0d4af
Masato Yoshioka <yoshioka0627@gmail.com>
2024-06-01 15:27:56 +0900

    libs

---
+++ lib/components/avatar.d.ts
@@ -0,0 +1,30 @@
+import * as React from "preact/compat"
+import { PrimitiveForwardRefComponent, ComponentPropsWithoutRef } from "../type-utils.ts"
+
+type PrimitiveSpanProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"span">>;
+
+interface AvatarProps extends PrimitiveSpanProps {
+}
+
+export const Root: React.ForwardRefExoticComponent<
+    AvatarProps & React.RefAttributes<HTMLSpanElement>
+>;
+
+interface AvatarFallbackProps extends PrimitiveSpanProps {
+    delayMs?: number;
+}
+
+export const Fallback: React.ForwardRefExoticComponent<
+    AvatarFallbackProps & React.RefAttributes<HTMLSpanElement>
+>;
+
+type ImageLoadingStatus = 'idle' | 'loading' | 'loaded' | 'error';
+type PrimitiveImageProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"img">>;
+
+interface AvatarImageProps extends PrimitiveImageProps {
+    onLoadingStatusChange?: (status: ImageLoadingStatus) => void;
+}
+
+export const Image: React.ForwardRefExoticComponent<
+    AvatarImageProps & React.RefAttributes<HTMLImageElement>
+>;
