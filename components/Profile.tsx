502afe8ec34ba0e13570b5ce66dfe3d731c748cc
Masato Yoshioka <yoshioka0627@gmail.com>
2024-06-01 15:42:42 +0900

    chore: Components 化

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,2 +1,15 @@
-const Profile = ({user}: User | null) => (
-)
+import { User } from "../lib/domain/User.ts";
+import { Avatar, AvatarFallback, AvatarImage } from "./Avatar.tsx";
+
+export const Profile = ( {user}: {user: User} ) => (
+  <>
+    <Avatar>
+      <AvatarImage src={user.avatar_url} alt={user.name} />
+      <AvatarFallback>{user.name}</AvatarFallback>
+    </Avatar>
+    <p class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+      name: {user.name}
+      email: {user.email}
+    </p>
+  </>
+);
