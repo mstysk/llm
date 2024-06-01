9340e971ce323f20237aad53c2669cd6523cab01
Masato Yoshioka <yoshioka0627@gmail.com>
2024-06-01 15:45:19 +0900

    chore: 不要なファイルを削除

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,8 +7,6 @@ import * as $_app from "./routes/_app.tsx";
 import * as $_layout from "./routes/_layout.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
 import * as $api_chat from "./routes/api/chat.ts";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $api_stream from "./routes/api/stream.tsx";
 import * as $callback from "./routes/callback.tsx";
 import * as $chat from "./routes/chat.tsx";
 import * as $error from "./routes/error.tsx";
@@ -16,7 +14,6 @@ import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $login from "./routes/login.tsx";
 import * as $logout from "./routes/logout.tsx";
-import * as $Counter from "./islands/Counter.tsx";
 import * as $Sidebar from "./islands/Sidebar.tsx";
 import * as $Stream from "./islands/Stream.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -28,8 +25,6 @@ const manifest = {
     "./routes/_layout.tsx": $_layout,
     "./routes/_middleware.ts": $_middleware,
     "./routes/api/chat.ts": $api_chat,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/api/stream.tsx": $api_stream,
     "./routes/callback.tsx": $callback,
     "./routes/chat.tsx": $chat,
     "./routes/error.tsx": $error,
@@ -39,7 +34,6 @@ const manifest = {
     "./routes/logout.tsx": $logout,
   },
   islands: {
-    "./islands/Counter.tsx": $Counter,
     "./islands/Sidebar.tsx": $Sidebar,
     "./islands/Stream.tsx": $Stream,
   },
